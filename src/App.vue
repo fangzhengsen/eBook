a848dd2ab117a293788e705397faa9f9d8810ba2
fzs <frank@ying31.com>
2019-12-16 18:10:36 +0800

    ebook init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {}
+document.addEventListener('DOMContentLoaded', () => {
+  // 获取html元素
+  const html = document.querySelector('html')
+  // 根据屏幕宽度动态计算fontSize
+  let fontSize = window.innerWidth / 10
+  // 当fontSize大于50时，设置最大值为50
+  fontSize = fontSize > 50 ? 50 : fontSize
+  // 设置html的fontSize属性，使rem生效，1rem = fontSize + 'px'
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
